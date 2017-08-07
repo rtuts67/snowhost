26cfd6e9d37dd133a5b0268e087ce4782892c66a
Rosemary Tuttle <rosemary.brinton@gmail.com>
2017-08-07 16:59:00 -0700

    Trying to add database for form submit

--- scripts/view/view.js
+++ scripts/view/view.js
@@ -7,5 +7,35 @@
   }
   skiHomeLoad();
 
+  var handleTheSubmitButton = function () {
+    $('#registerHere').on('click', function(event) {
+      event.preventDefault();
+      $('#registrationConfirmed').fadeIn(400);
+      var el = $(this);
+    })
+  }
+  handleTheSubmitButton();
+
+  var handleTheForm = function () {
+    $('#registerHere').submit(function(event) {
+      event.preventDefault();
+
+      var registerData = {
+        name: $(this).find('#name').val(),
+        email: $(this).find('#email').val(),
+        password: $(this).find('#password').val()
+      }
+
+      Member.postData(registerData)
+      // Member.postData(registerData);
+      // $('.page-content').hide();
+      // $('#registerHere').reset[0];
+      // $('#registrationConfirmed').fadeIn(500);
+
+    })
+  };
+
+  handleTheForm();
+
   module.skiHomeView = skiHomeView;
 })(window);
--- server.js
+++ server.js
@@ -2,6 +2,49 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+var pg = require('pg')
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432'
+
+app.get('/registerDB', function(req, res) {
+  const client = new pg.Client(connectionString);
+  client.connect(function(err) {
+    if(err) {
+      console.error('Trouble connecting to postgres', err)
+    }
+    client.query(
+      'SELECT * FROM register_data',
+      function(err, result) {
+        if(err) {
+          console.error('Query failed', err)
+        }
+        res.json(result);
+        client.end();
+      }
+    )
+  })
+})
+
+app.post('/registerDB', bodyParser.json(), function(req, res){
+  const client = new pg.Client(connectionString);
+
+  client.connect(function(err){
+    if(err) console.error('Trouble connecting to postgres', err)
+
+    client.query(
+    'INSERT INTO register_data (name, email, password) VALUES($1, $2, $3)',
+    [req.body.name, req.body.email, req.password],
+    function(err) {
+      if(err) console.error('Error running query', err);
+      client.end();
+    })
+  })
+  res.send('Post complete')
+})
+
 app.use(express.static('./'))
 
 function callBack(request, response) {
@@ -10,4 +53,4 @@ function callBack(request, response) {
 
 app.get('*', callBack)
 
-app.listen(PORT, () => console.log('Listening on port ' + PORT));
+app.listen(PORT, () => console.log('Listening on port ' + PORT + ' ' + connectionString));
